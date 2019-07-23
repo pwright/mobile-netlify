2d72691d6d2c32a66b0a6198739f45be512b0dae
Paul Wright <pwright@redhat.com>
2019-07-23 14:26:30 +0100

    update

--- bin/netlify-build.sh
+++ bin/netlify-build.sh
@@ -3,12 +3,14 @@ echo 'netlify-publish-adoc'
 node_modules/.bin/antora --pull ../antora-site.yml
 
 echo 'node-publish-adoc'
+pwd
 
 gem install asciidoctor
 
-for filename in ../modules/ROOT/pages/*.adoc; do
+
+for filename in modules/ROOT/pages/*.adoc; do
 
    echo $filename
-  ../bin/asciidoc-coalescer.rb $filename > ../build/site/$(basename "$filename")
+  ./bin/asciidoc-coalescer.rb $filename > ../build/site/$(basename "$filename")
 
 done
